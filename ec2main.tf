a958d84931629b109636f9e7d79d8589a9015a90
Shaik Shaheeda <112762527+SShaheeda@users.noreply.github.com>
2022-11-21 13:53:35 +0530

    Update ec2main.tf

--- ec2main.tf
+++ ec2main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git@github.com:SShaheeda/aws_tf_vpc.git"
+  source = "https://github.com/SShaheeda/aws_tf_vpc.git"
   vpc_cidr = var.ec2_vpc_cidr
   igw_cidr_block = var.ec2_igw_cidr_block
   public_subnet_cidr = var.ec2_public_subnet_cidr
